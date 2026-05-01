308514b4865655f51f25551e2827dc7c495d1879
Alexis-Ch15 <alexis.alexander.chasi@gmail.com>
2026-04-30 20:21:57 -0500

    Refactor calcularPagoFinal to use super method

--- Sistema de Gestion de Becas y Pagos Automaticos/BecadoExcelencia.java
+++ Sistema de Gestion de Becas y Pagos Automaticos/BecadoExcelencia.java
@@ -1,13 +1,13 @@
 public class BecadoExcelencia extends EstudianteBecado{
     private double bonoExcelencia;
 
-    public BecadoExcelencia(String nombre, String codigo, double promedio, double valorMatricula, double porcentajeBeca,double bonoExcenlencia){
+    public BecadoExcelencia(String nombre, String codigo, double promedio, double valorMatricula, double porcentajeBeca,double bonoExcelencia){
         super(nombre,codigo,promedio,valorMatricula,porcentajeBeca);
         setBonoExcelencia(bonoExcelencia);
     }
     @Override
     public double calcularPagoFinal(){
-        return ((bonoExcelencia+ getPorcentajeBeca())/100) * getValorMatricula() - getValorMatricula();
+        return super.calcularPagoFinal() - bonoExcelencia;
     }
     public void setBonoExcelencia(double bonoExcelencia){
         if(bonoExcelencia > 0){
