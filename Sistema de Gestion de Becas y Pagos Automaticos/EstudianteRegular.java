ddb661c01cf6ec98391eaa86499abb1113a9e2f1
Alexis-Ch15 <alexis.alexander.chasi@gmail.com>
2026-04-30 20:24:49 -0500

    Refactor EstudianteRegular to RegularConRecargo class

--- Sistema de Gestion de Becas y Pagos Automaticos/EstudianteRegular.java
+++ Sistema de Gestion de Becas y Pagos Automaticos/EstudianteRegular.java
@@ -1,20 +1,19 @@
-public class EstudianteRegular extends Estudiante {
-    private int numeroMaterias;
-    public EstudianteRegular(String nombre, String codigo, double promedio, double valorMatricula, int numeroMaterias) {
-        super(nombre, codigo, promedio, valorMatricula);
-        setNumeroMaterias(numeroMaterias);
+public class RegularConRecargo extends EstudianteRegular{
+    private double recargoPendiente;
+    public RegularConRecargo(String nombre,String codigo,double promedio,double valorMatricula,int numeroMaterias,double recargoPendiente){
+        super(nombre,codigo,promedio,valorMatricula,numeroMaterias);
+        setRecargoPendiente(recargoPendiente);
     }
-
     @Override
     public double calcularPagoFinal(){
-        return getValorMatricula() + (numeroMaterias * 10);
+        return super.calcularPagoFinal() + recargoPendiente;
     }
-    public int getNumeroMaterias(){
-        return numeroMaterias;
-    }
-    public void setNumeroMaterias(int numeroMaterias){
-        if(numeroMaterias > 0){
-            this.numeroMaterias = numeroMaterias;
+    public void setRecargoPendiente(double recargoPendiente){
+        if(recargoPendiente > 0){
+            this.recargoPendiente = recargoPendiente;
         }
+    }
+    public double getRecargoPendiente(){
+        return recargoPendiente;
     }
 }
